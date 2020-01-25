8400867e11fdfddaf36bc47e56ce487f5c9c5029
cobaltway <toutcobalt@gmail.com>
2020-01-25 21:42:33 +0100

    cleanup index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <section class="section">
-    Hello, world!
+  <section class="section content">
+    <ul>
+      <li>
+        <a href="https://ecorats.org"> Pour un écologisme critique et fondé sur les sciences. </a>
+      </li>
+      <li>
+        <nuxt-link to="/ges-alim">
+          Quel impact de l'alimentation sur les émissions de gaz à effet de serre ?
+        </nuxt-link>
+      </li>
+    </ul>
   </section>
 </template>
 
